743a3e438fc79227bac9a6ff855fb00ce691635b
Frank Tonya <francktonya7@gmail.com>
2025-04-21 09:33:20 +0200

    Initialisation du projet

---
+++ src/main/java/org/projet/gestionemploisdutemps/GestionEmploisDuTempsApplication.java
@@ -0,0 +1,13 @@
+package org.projet.gestionemploisdutemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmploisDuTempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmploisDuTempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/projet/gestionemploisdutemps/GestionEmploisDuTempsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projet.gestionemploisdutemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmploisDuTempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
